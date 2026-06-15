f4d66784ae5239eee4c3b15fa1dba37f5d1f1c68
AminNavabzade <aminnavabzade@gmail.com>
2026-06-15 20:51:08 +0330

    add switch case

--- 01_Basics/04_Condition.cpp
+++ 01_Basics/04_Condition.cpp
@@ -29,6 +29,68 @@ int main(){
     cout<<"you can'n drive!!!!!!!!!!!\n";
   }
 
+ // switch - cass ==>> for long cindition
+  // switch(vriable){
+     // case - condition:
+        // ## code
+        // break
+     // case - condition:
+        // ## code;
+        //break
+     // case - condition:
+        // ## code
+        // break
+     // case - condition:
+        // ## code;
+        // break
+     // default :
+        // ## code; 
+  //}
+  int mount;
+  cout<<"\ntell me your mount:\n";
+  cin>>mount;
+  switch (mount){
+    case 1:
+      cout<<"this mount have 31 days.";
+      break;
+    case 2:
+      cout<<"this mount have 31 days.";
+      break;
+    case 3:
+      cout<<"this mount have 31 days.";
+      break;
+    case 4:
+      cout<<"this mount have 29 days.";
+      break;
+    case 5:
+      cout<<"this mount have 31 days.";
+      break;
+    case 6:
+      cout<<"this mount have 30 days.";
+      break;
+    case 7:
+      cout<<"this mount have 30 days.";
+      break;
+    case 8:
+      cout<<"this mount have 30 days.";
+      break;
+    case 9:
+      cout<<"this mount have 30 days.";
+      break;
+    case 10:
+      cout<<"this mount have 30 days.";
+      break;
+    case 11:
+      cout<<"this mount have 30 days.";
+      break;
+    case 12:
+      cout<<"this mount have 30 days.";
+      break;
+    default:
+      cout<<"your mount is wrong...........";  
+  
+  }
+
 
 return 0;
 }
