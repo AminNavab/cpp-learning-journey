2e4aa3d60a790ec391b44e236ea422cba72270f4
AminNavabzade <aminnavabzade@gmail.com>
2026-06-29 01:15:50 +0330

    add enum part 2

--- 03_basicDataStructure/03_Enum.cpp
+++ 03_basicDataStructure/03_Enum.cpp
@@ -14,3 +14,91 @@ using namespace std;
     // change values:
     //enum day{saturday_1 = 10,sunday_1 = 11, monday_1 = 12, tuesday_1 = 13, wednesdat_1 = 14, thursday_1 = 15, friday_1 = 16};
     //enum day{saturday_2 = 20,sunday_2, monday_2, tuesday_2, wednesdat_2, thursday_2, friday_2};
+
+    int main(){
+    // assign an enum:
+    day today = friday;
+    enum day tomarow = saturday;
+    // use an enum:
+    // type(1)
+    switch (today){
+        case saturday: cout<< "it's saturday...\n";
+            break;
+        case sunday: cout<< "it's sunday...\n";
+            break;
+        case monday: cout<< "it's monday...\n";
+            break;
+        case tuesday: cout<< "it's tuesday...\n";
+            break;
+        case wednesdat: cout<< "it's wednesday...\n";
+            break;
+        case thursday: cout<< "it;s thursday...\n";
+            break;
+        case friday: cout<< "it's friday...\n";
+            break;
+    }
+    switch (today){
+        case 0: cout<< "it's saturday...\n";
+            break;
+        case 1: cout<< "it's sunday...\n";
+            break;
+        case 2: cout<< "it's monday...\n";
+            break;
+        case 3: cout<< "it's tuesday...\n";
+            break;
+        case 4: cout<< "it's wednesday...\n";
+            break;
+        case 5: cout<< "it's thursday.....\n";
+            break;
+        case 6: cout<< "it's friday...\n";
+            break;
+    }
+    switch (tomarow){
+        case saturday: cout<< "it's saturday...\n";
+            break;
+        case sunday: cout<< "it's sunday...\n";
+            break;
+        case monday: cout<< "it's monday...\n";
+            break;
+        case tuesday: cout<< "it's tuesday...\n";
+            break;
+        case wednesdat: cout<< "it's wednesday...\n";
+            break;
+        case thursday: cout<< "it;s thursday...\n";
+            break;
+        case friday: cout<< "it's friday...\n";
+            break;
+    }
+    switch (tomarow){
+        case 0: cout<< "it's saturday...\n";
+            break;
+        case 1: cout<< "it's sunday...\n";
+            break;
+        case 2: cout<< "it's monday...\n";
+            break;
+        case 3: cout<< "it's tuesday...\n";
+            break;
+        case 4: cout<< "it's wednesday...\n";
+            break;
+        case 5: cout<< "it's thursday.....\n";
+            break;
+        case 6: cout<< "it's friday...\n";
+            break;
+    }
+    // type(2)
+    enum day day = wednesdat;
+    enum day day_1 = saturday;
+    cout<< "day is "<<day<<endl;
+    cout<< "day_1 is "<<day_1<<endl;
+    cout<< "**********************\n";
+    // change values:
+    enum day day_a = saturday;
+    cout<<day_a;
+
+
+    
+
+
+
+    return 0;
+}
