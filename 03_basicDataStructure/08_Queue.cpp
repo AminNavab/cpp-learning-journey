55e27e3f752ea48da9da80bac7d6d8a464986af7
AminNavabzade <aminnavabzade@gmail.com>
2026-07-04 16:14:19 +0330

    add queue part 2

--- 03_basicDataStructure/08_Queue.cpp
+++ 03_basicDataStructure/08_Queue.cpp
@@ -30,5 +30,35 @@ int main(){
     // volvo
     // tesla (back (last) element)
     cout<<"***************************\n";
+      // access queue element :
+    // NOTE : You cannot access queue elements by referring to index numbers, like you would with arrays and vectors.
+    //               In a queue, you can only access the element at the front or the back, using .front() and .back() respectively.
+    cout<< cars_1.front()<<endl;
+    cout<< cars_1.back()<<endl;
+    cout<<"********************************\n";
+    // Change Front and Back Elements
+    // You can also use .front and .back to change the value of the front and back elements:
+    cars_1.front() = "bmw_1";
+    cars_1.back() = "tesla_1";
+    cout<< cars_1.front()<<endl;
+    cout<< cars_1.back()<<endl;
+    cout<<"********************************\n";
+    // remove queue element :
+    // You can use the .pop() function to remove an element from the queue.
+    //         This will remove the front element (the first and oldest element that was added to the queue).
+    cars_1.pop();
+    cout<< cars_1.front()<<endl;
+    cout<< cars_1.back()<<endl;
+    cout<<"********************************\n";
+    // queue size :
+    // queue_name .size();
+    cout<< cars_1.size()<<endl;
+    cout<<"********************************\n";
+    // Check if the Queue is Empty :
+    // queue_name .empty();
+    queue<string>cars_2;
+    cout<< cars_1.empty()<<endl;
+    cout<< cars_2.empty()<<endl;
+    cout<<"********************************\n";
     return 0;
 }
