f22d1b0009eba135a050585c33393f0f0091bf2c
AminNavabzade <aminnavabzade@gmail.com>
2026-07-13 23:50:50 +0330

    add map part 2

--- 03_basicDataStructure/11_Map.cpp
+++ 03_basicDataStructure/11_Map.cpp
@@ -58,5 +58,49 @@ int main(){
     cout<< numbers_2.at("three")<< endl;
     cout<< numbers_2.at("four")<< endl;
     cout<<"**************************************\n";
+     // remove map element :
+    // map_name .erase(key);
+    numbers_1.erase("four");
+    numbers_2.erase("four");
+    // cout<< numbers_1["four"]<<endl;          (error!!!!)
+    // cout<< numbers_2.at("four")<< endl;       (error!!!!)
+
+    // clear all map :
+    // map_name .clear();
+    numbers_1.clear();
+    cout<<"**************************************\n";
+    // map size :
+    // map_name .size();
+    cout<< numbers_1.size()<<endl;
+    cout<< numbers_2.size()<<endl;
+    cout<<"**************************************\n";
+    // Check if a Map is Empty :
+    // map_name .empty();
+    cout<< numbers_1.empty()<<endl;
+    cout<< numbers_2.empty()<<endl;
+    cout<<"**************************************\n";
+    // serch in map :
+    // map_name .count(key);     (true or false)
+    cout<< numbers_2.count("four")<<endl;
+    cout<< numbers_2.count("one")<<endl;
+    cout<< numbers_2.count("two")<<endl;
+    cout<<"**************************************\n";
+    // show map :
+    // (1) foreach = You can loop through a map with the for-each loop. However, there are a couple of things to be aware of:
+    // (#) You should use the auto keyword (introduced in C++ version 11) inside the for loop.
+    //        This allows the compiler to automatically determine the correct data type for each key-value pair.
+    // (##) Since map elements consist of both keys and values, you have to include .first to access the keys, and .second to access values in the loop.
+    // (###) Elements in the map are sorted automatically in ascending order by their keys:
+    for(auto num : numbers_2){
+        cout<< num.first << " is "<< num.second<< endl;
+    }
+    cout<<"-----------------------------\n";
+    // (2) If you want to reverse the order, you can use the greater<type> functor inside the angle brackets, like this :
+    //        *****( Based on the keys )*****
+    map<string, int, greater<string>>numbers_3 = {{"one", 1},{"two", 2},{"three", 3}};
+    for(auto num : numbers_3){
+        cout<< num.first << " is "<< num.second<< endl;
+    }
+    cout<<"******************************\n";
     return 0;
 }
