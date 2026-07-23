6e838640c6c2879e6c17bf3730337f460793c0d7
AminNavabzade <aminnavabzade@gmail.com>
2026-07-23 21:40:06 +0330

    add project 9 part 2

--- 04_Projects/MiniProjects/09.cpp
+++ 04_Projects/MiniProjects/09.cpp
@@ -69,5 +69,107 @@ int main(){
     z++;
     }
     cout<<"---------------------\n";
+        // number (4):
+    // h = 5
+    int m = 0;
+    int m_1 = (h*2)-1; // 9
+    int m_2 = 0;
+    for(int i = 1; i<=h; i++){
+        for(int j = 1; j<=m_1; j++){
+            if(i==1){
+                if(j<h || j>h){
+                    cout<<"_ ";
+                }
+                else{
+                    cout<<"o ";
+                }
+            }
+            if(i>1 && i<h){
+                if(j<h-m || j>h+m){  // (5+m < j < 5-m)
+                    cout<<"_ ";
+                }
+                else if(j==h || (j<h && j>h-m_2) || (j>h && j<h+m_2)){
+                    cout<<". ";
+                }
+                else{
+                    cout<<"o ";
+                }
+            } 
+            if(i==h){
+                cout<<"o ";
+            }    
+        }
+        cout<<endl;
+    m++;
+    m_2++;
+    } 
+    cout<<"---------------------\n";
+
+    // square : 5x5
+    // number (1):
+    int length = 5;
+    int width = 5;
+    for(int i = 1; i<=width; i++){
+        for(int j = 1; j<=length; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    cout<<"---------------------\n";
+    // number (2):
+    for(int i = 1; i<=width; i++){
+        for(int j = 1; j<=length; j++){
+            if(i==1 || i==width){
+                cout<<"o ";
+            }
+            else if(j>1 && j<length){
+                cout<<". ";
+            }
+            else if(j==1 || j==length){
+                cout<<"o ";
+            }
+        }
+        cout<<endl;
+    }
+    cout<<"---------------------\n";
+
+    // Diamond
+    // Small diameter = 5
+    // Large diameter = 5
+    // (a && A) :: 2k+1
+    int a = 5;
+    int A = 5;
+    int y = (a/2)+1; // 3
+    int Y = (A/2)+1; // 3
+    int u = 0;
+    int u_1 = Y+1; // 1
+    for(int i = 1; i<=A; i++){
+        for(int j = 1; j<=a; j++){
+            if(i==Y){
+                cout<<"o ";
+            }
+            if(i<Y){
+                if(j<y-u || j>y+u){
+                    cout<<". ";
+                }
+                else{
+                    cout<<"o ";
+                }
+            }    
+            if(i>Y){
+                if(j<(y-u_1) || j>(y+u_1)){
+                    cout<<". ";
+                }
+                else{
+                    cout<<"o ";
+                }
+            }    
+            
+        }
+        u++; 
+        u_1--;
+        cout<<endl;  
+    }
+    cout<<"---------------------\n";
     return 0;
 }
